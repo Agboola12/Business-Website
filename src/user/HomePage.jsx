7a4082d8ad691ef31dd4f11cb9bf4e7e05b79164
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-21 16:19:43 +0100

    wip on landing page

--- src/user/HomePage.jsx
+++ src/user/HomePage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import NavBar from './NavBar';
-import land from '../assets/01.jpg'
+// import style from './Homepage.module.css'
+import style from '../user/HomePage.module.css'
+// import land from '../assets/01.jpg'
 
 
 const HomePage = () => {
@@ -20,10 +22,16 @@ const HomePage = () => {
             </div>
             <div className='w-1/2   p-5'>
               <img src={land} alt='altt' className='h-[500px] rounded-tl-[70px] rounded-br-[70px] w-full aspect-auto object-cover ' />
-            </div>
-          </div>
-        </div>
-      </section> */}
+              </div>
+              </div>
+              </div>
+            </section> */}
+<section className="h-[500px]  w-[100%] aspect-auto object-cover" id={style.section} >
+  <div className='text-center text-white mt-[10em]'>
+  <h3 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-6">Experience the future today.</h3>
+              <p className="text-lg md:text-xl mb-8">Explore our range of phones and laptops crafted to inspire and delight.</p>
+  </div>
+</section>
 
 
     </div>
